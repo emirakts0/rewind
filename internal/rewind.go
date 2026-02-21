ec3563ee49004d528e0d5c19f4681ef6ac832f4b
emirakts0 <emiraktas143@gmail.com>
2026-02-21 23:09:00 +0300

    refactor string pointer conversion functions and move to utils

--- internal/rewind.go
+++ internal/rewind.go
@@ -4,9 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log/slog"
-	"strings"
 	"syscall"
-	"unsafe"
 )
 
 // --- DLL Loading ---
@@ -135,35 +133,6 @@ type ReplayStatus struct {
 	DiskUsage    uint64  `json:"disk_usage_bytes"`
 	MemoryUsage  uint64  `json:"memory_usage_bytes"`
 }
-
-// --- Helpers ---
-
-func ptrToString(ptr uintptr) string {
-	if ptr == 0 {
-		return ""
-	}
-	var b []byte
-	p := unsafe.Pointer(ptr)
-	for {
-		val := *(*byte)(p)
-		if val == 0 {
-			break
-		}
-		b = append(b, val)
-		p = unsafe.Pointer(uintptr(p) + 1)
-	}
-	return string(b)
-}
-
-func stringToPtr(s string) uintptr {
-	if !strings.HasSuffix(s, "\x00") {
-		s += "\x00"
-	}
-	b := []byte(s)
-	return uintptr(unsafe.Pointer(&b[0]))
-}
-
-// --- API Functions ---
 
 type MonitorsResult struct {
 	Success bool             `json:"success"`
